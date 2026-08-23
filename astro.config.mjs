c5d88a1fdc0ae1c6ed777b7d856d9e59dc0a4a0e
“diamonds128” <diamonds128@users.noreply.github.com>
2026-08-23 21:44:50 +0800

    chore: 在导航栏添加图库页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig({
         {
           label: '其他',
           items: [
+			{ label: '图库', link: '/gallery' },
             { label: '服务端模组列表', link: '/mods' },
             { label: '炸服事件', link: '/incident' },
             { label: '许可证', link: '/license' },
